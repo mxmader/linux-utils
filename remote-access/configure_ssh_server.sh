ea6564d84f2c6232ebfbdc167beaf13751732bd7
Joe Mader <jmader@jmader.com>
2022-01-30 16:18:55 -0700

    port again

--- remote-access/configure_ssh_server.sh
+++ remote-access/configure_ssh_server.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+SSH_PORT=6666
+
 sudo ufw enable
 
 sudo apt install -y fail2ban crudini openssh-server
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 
 # configure SSH server options + security by obscurity
-sudo sed -i -e "s/^#*Port.*$/Port 6666/" /etc/ssh/sshd_config
+sudo sed -i -e "s/^#*Port.*$/Port ${SSH_PORT}/" /etc/ssh/sshd_config
 sudo sed -i -e "s/^#*PermitRootLogin.*$/PermitRootLogin no/" /etc/ssh/sshd_config
 sudo sed -i -e "s/^#*PasswordAuthentication.*$/PasswordAuthentication no/" /etc/ssh/sshd_config
 sudo systemctl restart ssh
@@ -24,4 +26,4 @@ sudo systemctl restart fail2ban
 # Set basic firewall rule(s)
 # disabled - uses port 22
 # sudo ufw allow ssh
-sudo ufw allow from any proto tcp to any port 666
+sudo ufw allow from any proto tcp to any port "${SSH_PORT}"
