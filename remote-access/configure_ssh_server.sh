99fed9b172f078530ad00fa4c8c49b26d70eca66
Joe Mader <jmader@jmader.com>
2022-01-30 16:13:04 -0700

    ssh server

--- remote-access/configure_ssh_server.sh
+++ remote-access/configure_ssh_server.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-sudo apt-get install -y fail2ban crudini
+sudo ufw enable
+
+sudo apt install -y fail2ban crudini openssh-server
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+# configure SSH server options + security by obscurity
+sudo sed -i -e "s/^#*Port.*$/Port 666/" /etc/ssh/sshd_config
+sudo sed -i -e "s/^#*PermitRootLogin.*$/PermitRootLogin no/" /etc/ssh/sshd_config
+sudo sed -i -e "s/^#*PasswordAuthentication.*$/PasswordAuthentication no/" /etc/ssh/sshd_config
+sudo systemctl restart ssh
 
 # only give clients one try at authentication. only true way to keep the bad guys out.
 sudo crudini --set /etc/fail2ban/jail.local DEFAULT bantime -1
@@ -14,5 +22,6 @@ sudo systemctl restart fail2ban
 # fail2ban-client set sshd unbanip <client IP>
 
 # Set basic firewall rule(s)
-ufw allow ssh
-ufw enable
+# disabled - uses port 22
+# sudo ufw allow ssh
+sudo ufw allow from any proto tcp to any port 666
