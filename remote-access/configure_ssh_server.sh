5f7ab4694a3ea627042d8b29e4111a54753f1a27
Joe <jmader@jmader.com>
2023-07-31 06:12:37 -1000

    order of ops

--- remote-access/configure_ssh_server.sh
+++ remote-access/configure_ssh_server.sh
@@ -2,12 +2,22 @@
 
 set -e
 
-SSH_PORT=6666
+export SSH_PORT=6666
 
 sudo ufw enable
 
 sudo apt-get install -y fail2ban crudini openssh-server
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+
+# Set basic firewall rule(s)
+# disabled - uses port 22
+# sudo ufw allow ssh
+sudo ufw allow from any proto tcp to any port "${SSH_PORT}"
+
+# only give clients one try at authentication. only true way to keep the bad guys out.
+sudo crudini --set /etc/fail2ban/jail.local DEFAULT bantime -1
+sudo crudini --set /etc/fail2ban/jail.local DEFAULT maxretry 1
+sudo systemctl restart fail2ban
 
 # configure SSH server options + security by obscurity
 sudo sed -i -e "s/^#*Port.*$/Port ${SSH_PORT}/" /etc/ssh/sshd_config
@@ -15,15 +25,5 @@ sudo sed -i -e "s/^#*PermitRootLogin.*$/PermitRootLogin no/" /etc/ssh/sshd_confi
 sudo sed -i -e "s/^#*PasswordAuthentication.*$/PasswordAuthentication no/" /etc/ssh/sshd_config
 sudo systemctl restart ssh
 
-# only give clients one try at authentication. only true way to keep the bad guys out.
-sudo crudini --set /etc/fail2ban/jail.local DEFAULT bantime -1
-sudo crudini --set /etc/fail2ban/jail.local DEFAULT maxretry 1
-sudo systemctl restart fail2ban
-
 # to unban a given IP if you make a mistake during this procedure, run this via console:
 # fail2ban-client set sshd unbanip <client IP>
-
-# Set basic firewall rule(s)
-# disabled - uses port 22
-# sudo ufw allow ssh
-sudo ufw allow from any proto tcp to any port "${SSH_PORT}"
